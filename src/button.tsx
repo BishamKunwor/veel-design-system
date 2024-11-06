b159a7587da9287a95b6518ce2f6ec500aed1c9f
Bisham Kunwor <novelian.nova@gmail.com>
2024-11-06 09:07:11 +0545

    feat: Add TailwindCss config files

--- src/button.tsx
+++ src/button.tsx
@@ -1,3 +1,9 @@
+import React from 'react';
+
 export default function Button() {
-  return <button>This is a button</button>;
+  return (
+    <button className='bg-red-400 text-4xl font-medium'>
+      This is a button okay
+    </button>
+  );
 }
